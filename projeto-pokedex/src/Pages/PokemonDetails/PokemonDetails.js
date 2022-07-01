1a309ccc59e668f20d8d6a5baccbaa520842f566
DiogoB0mfim <diogohbomfim@gmail.com>
2022-07-01 17:27:22 -0300

    Criei um componente para a lógica e design dos detalhes

--- projeto-pokedex/src/Pages/PokemonDetails/PokemonDetails.js
+++ projeto-pokedex/src/Pages/PokemonDetails/PokemonDetails.js
@@ -1,51 +1,26 @@
-import { useEffect, useState } from 'react';	
 import { useNavigate } from "react-router";
-import { useParams } from 'react-router';
-import axios from "axios"
-import {BASE_URL} from "../../Constants/url"
+import PokemonDetailsCards from '../../Components/PokemonDetailsCards';
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  background: red;
+  align-items: center;
+  justify-content: center;
+`;
 
 const PokemonDetails = () => {
   const navigate = useNavigate()
-  const { name } = useParams()
-  const [selectedPokemon, setSelectedPokemon] = useState([])
-    
-  useEffect(() => {axios.get(`${BASE_URL}/pokemon/${name}`) .then((response) => {setSelectedPokemon(response.data)})}, [])
   
-  const renderPokemonStats = selectedPokemon.stats && selectedPokemon.stats.map((stat) => {
-    return <div key={stat.stat.name}>
-              <p>{stat.stat.name}</p>
-              <p>{stat.base_stat}</p>
-          </div>
-  })
-     
-  const renderPokemonMoves = selectedPokemon.moves && selectedPokemon.moves.map((move, index) => {
-    if (index < 5) {  
-      return <p key={index}>{move.move.name}</p>
-  }})
-
-  const renderPokemonType = selectedPokemon.types && selectedPokemon.types.map((type, index) => {
-    return <div key={index}>
-              <p>{type.type.name}</p>
-          </div>
-  })
-    
   return (
     <div>
-      <h1>Detalhes do pokemon</h1>
-      <button onClick={() => navigate("/")}>Voltar para lista de pokemons</button>
-      <button onClick={() => navigate("/pokedex")}>Voltar para a pokedex</button>
-      {selectedPokemon && selectedPokemon.sprites && (
-        <div>
-          <img src={selectedPokemon.sprites.versions["generation-v"]["black-white"].animated.front_default}/>
-          <img src={selectedPokemon.sprites.versions["generation-v"]["black-white"].animated.back_default}/>
-        </div>   
-      )}
-      <h2>Status</h2>
-        {renderPokemonStats}
-      <h2>Tipo</h2>
-        {renderPokemonType}
-      <h2>Poderes</h2>
-        {renderPokemonMoves}
+      <Header>
+        <img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png"/>
+        <button onClick={() => navigate("/")}>Voltar para lista de pokemons</button>
+        <button onClick={() => navigate("/pokedex")}>Voltar para a pokedex</button>
+      </Header>
+      <PokemonDetailsCards/>
+      
     </div>
   )}
 
